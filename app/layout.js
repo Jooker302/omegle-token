f816396b2c8600b0a223a2486b2c0da6a0912e8a
jooker302 <i.omer6637@gmail.com>
2023-08-17 09:16:10 -0700

    P

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Omegle Token',
-  description: 'Omegle Token (DxSale)',
-}
+  title: "Omegle Token",
+  description: "Omegle Token (DxSale)",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="main.jpeg" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
